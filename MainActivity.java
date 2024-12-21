f6ae78c50d2abec0b144809a302608efa9ac88e9
Himanshu P Dev <153818164+himanshupdev123@users.noreply.github.com>
2024-12-21 17:21:14 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,33 @@
+package tf.iitb.myprofile;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+Button launchBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        //loading views
+        launchBtn=findViewById(R.id.viewwork_button);
+        //setting listners
+        launchBtn.setOnClickListener(view -> {
+            Intent in = new Intent(getApplicationContext(), ViewWorkActivity.class);
+            startActivity(in);
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
---
+++ ViewWorkActivity.java
@@ -0,0 +1,31 @@
+package tf.iitb.myprofile;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ViewWorkActivity extends AppCompatActivity {
+Button homeBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_view_work);
+        homeBtn=findViewById(R.id.home_btn);
+        homeBtn.setOnClickListener(view -> {
+            Intent in =new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(in);
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
